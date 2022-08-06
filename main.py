19333ee1af4129531bc7d0a18521af7242cd21d7
jrpinzong <jrpinzong@hotmail.com>
2022-08-05 19:38:04 -0500

    En main.py se agrego las path operation de las funciones C.R.U.D

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi import status, Response
+from fastapi import Body, Path
 
-from fastapi import status
+import models
+import db
 
 
 app = FastAPI()
@@ -29,3 +32,92 @@ def home():
 
             }
   
+@app.post(path="/users/",
+          response_model=models.UserBase,
+          status_code=status.HTTP_201_CREATED,
+          summary= "Create a User",
+          tags=["Users"]
+        )
+def create_a_user(user_data: models.UserBase = Body(...)):
+    
+    row = user_data.dict()
+    data = db.insertUser(row)
+    return data
+
+
+@app.get(path="/users/{user_id}",
+          response_model=models.User,
+          status_code=status.HTTP_202_ACCEPTED,
+          summary= "Show a User",
+          tags=["Users"]
+        )
+def read_a_user(user_id: int = Path(...,
+                            gt=0,
+                            title="User ID",
+                            description="This is the user id. It's requered"
+                            ), response : Response = status.HTTP_202_ACCEPTED
+                ):
+
+    row = db.readUser(user_id)
+    data = {}
+    if row is not None:
+        data["id"]= row[0]
+        data["name"]= row[1]
+        data["last_name"]= row[2]
+        data["birth_date"]= row[3]
+        data["email"]= row[4]
+        data["country"]= row[5]
+    else:
+        data["id"]= user_id
+        data["name"]= "None"
+        data["last_name"]= "None"
+        data["birth_date"]= "1950-01-01"
+        data["email"]= "None@None.none"
+        data["country"]= "None"
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return data
+
+
+@app.put(path="/users/{user_id}/update",
+          response_model=models.User,
+          status_code=status.HTTP_202_ACCEPTED,
+          summary= "Update a User",
+          tags=["Users"]
+        )
+def update_a_user(user_id: int = Path(...,
+                            gt=0,
+                            title="User ID",
+                            description="This is the user id. It's requered"
+                            ),
+                  user_data: models.UserBase = Body(...)
+                ):
+    row = user_data.dict()
+
+    row["id"] = user_id
+    data = db.updateUser(row)
+
+    return data
+
+
+@app.delete(path="/users/{user_id}/delete",
+          status_code=status.HTTP_202_ACCEPTED,
+          summary= "Delete a User",
+          tags=["Users"]
+@app.post(path="/users/",
+          response_model=models.UserBase,
+          status_code=status.HTTP_201_CREATED,
+          summary= "Create a User",
+          tags=["Users"]
+        )
+def create_a_user(user_data: models.UserBase = Body(...)):
+    
+        )
+def delete_a_user(user_id: int = Path(...,
+                            gt=0,
+                            title="User ID",
+                            description="This is the user id. It's requered"
+                            )
+                ):
+    row = db.deleteUser(user_id)
+    return row
+     
